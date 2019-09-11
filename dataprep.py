13d849a5780e6c4e5f73cbb5d460a7877c25ebc5
Akshay Kale <akshaykale915@gmail.com>
2019-09-10 19:01:23 -0500

    added function convertlisttostring

--- dataprep.py
+++ dataprep.py
@@ -3,9 +3,30 @@ Description:
     Something fun is about to happen
 
 Author: Akshay S. Kale
-Python Version: 
+Python Version: 3.6.4
 
 """
 
 import pandas as pd
 import numpy as numpy
+
+class DataPrep():
+    """
+     Description: The class contain functions to clean, process, and tranform the dataset.
+
+    """
+
+    def __init__(self):
+        pass
+
+    def convertStringListtoList(List):
+        """ 
+        description: The function converts list of string into a list of list.
+        
+        input-type: list - [list]
+        return-type: list - [list]
+
+        """
+        return  [int(item.strip(" ").strip("'")) for item in List[1:-1].split(',')]
+
+
