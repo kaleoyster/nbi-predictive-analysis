0cebdad689f77ccd408fc45655890e4177e08a45
Akshay Kale <akshaykale915@gmail.com>
2019-10-02 10:18:55 -0500

    made changes in the data processing file

--- datachef.py
+++ datachef.py
@@ -2,6 +2,38 @@
 Description:
     This script will contain functions to create a timeseries format of the NBI data. This script will also, intergrate other datasources like precipitation dataset from CDC, population dataset, and weather data (number of snowfall and number of freeze thaw) from infobridge.com
 
+The idea is to create a dish, which is appetitzing, 
+To break the monotony we make refer to things differently:
+    1. inital datafile = ingridients 
+    2. final datafile = dish
+    3. function = cook
+    4. preprocess = 
+    5. chop
+    6. Mix =
+    7. Garnish = 
+    9. Season = mapping functions
+    10. plotting functions = presentation
+
+
+gathering data, combine, season, 
+
+process 1 is creating a time-series dataset.
+    1. require all the functions to prepare timeseries data
+    2. 
+
+
+
+1.  CreateTimeseries
+2.  CreateTimeseriesLifeCycle
+3.  retDeckProctectionName
+4.  Mappingfunctions
+5.  
+
+Creating cookbook is basically creating a pipeline -> 
+
+Limitation or Things to add:
+    This function are not thoroughly checked using unit testing. If want to make sure these functions work all the time, you're welcome to join this adventure to make this cookbook.
+
 Author: Akshay S. Kale
 Python Version: 3.6.4
 
@@ -18,7 +50,8 @@ class DataChef():
 
     def __init__(self):
         pass
-
+   
+    def gather
     def convertStringListtoList(List):
         """ 
         description: The function converts list of string into a list of list.
